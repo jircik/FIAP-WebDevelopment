99a23d859d823948e03a88b0dccde77b367a7cc3
jircik <oarthurjircik@gmail.com>
2026-04-28 11:22:39 -0300

    feat: using methods includes, trim, split e join

---
+++ Aula 7 - 28-04-2026/src/js/index.js
@@ -0,0 +1,23 @@
+const email="                   oarthurjircik@gmail.com             ";
+const emailLimpo = email.trim(); //remove os espaços do início e fim
+
+console.log(emailLimpo);
+
+if(emailLimpo.includes("@")){ //verifica se o caractere existe na string
+    console.log("Email Valido");
+} else {
+    console.log("Email invalido");
+}
+
+
+const tituloArtigo = "Como aprender Java RAIZ";
+
+const texto1 = tituloArtigo.toUpperCase();
+console.log(texto1);
+
+const texto2 = tituloArtigo.split(" "); //divide a string em um array a cada ocorrencia do caractere selecionado
+console.log(texto2);
+
+const texto3 = texto2.join(" "); //junta um string[] em uma string separando os indices pelo caractere selecionado
+
+
